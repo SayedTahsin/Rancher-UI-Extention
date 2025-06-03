62f22fe5415fdcd69408b162901c4ab8b9aa059e
SayedTahsin <tahsin@appscode.com>
2025-06-03 15:27:04 +0600

    add Helm chart files and templates for my-chart; implement service, deployment, ingress, and HPA configurations; update Vuex action to use axios for data fetching

--- pkg/elemental/store/mydata/actions.ts
+++ pkg/elemental/store/mydata/actions.ts
@@ -1,11 +1,10 @@
-import $axios from '@bytebuilders/ui-modules/src/plugins/axios'
-
+import axios from "axios";
 
 export default {
   async fetchMyData({ commit }: any) {
     commit('setLoading', true);
     try {
-      const resp = await $axios.get('https://randomuser.me/api/',{withCredentials: false});
+      const resp = await axios.get('https://randomuser.me/api/');
       commit('setMyData', resp.data);
     } catch (error) {
       console.error('Error fetching data:', error);
