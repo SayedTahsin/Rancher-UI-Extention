169bc1bc4462f0f0e0aefc28b8bd9c215b06245c
SayedTahsin <tahsin@appscode.com>
2025-06-16 15:11:09 +0600

    Add light theme styles, SUSE theme, and normalize CSS; update test component with LabeledInput

--- pkg/testing/component/testComponent-custom.vue
+++ pkg/testing/component/testComponent-custom.vue
@@ -1,21 +1,50 @@
 <script setup lang="ts">
-import { computed, onMounted } from "vue";
-import { useStore } from 'vuex'
+import LabeledInput from "../assets/components/Form/LabeledInput/LabeledInput.vue";
+import { computed, onMounted, ref } from "vue";
+import { useStore } from "vuex";
 
-const store = useStore()
+const store = useStore();
 
-const userData = computed(() => store.getters['test-extension/userData']);
-const loading = computed(() => store.getters['test-extension/isLoading']);
+const userData = computed(() => store.getters["test-extension/userData"]);
+const loading = computed(() => store.getters["test-extension/isLoading"]);
 
-onMounted(()=>{
-  store.dispatch('test-extension/fetchMyData')
-})
+onMounted(() => {
+  store.dispatch("test-extension/fetchMyData");
+});
 
+const name = ref("");
 </script>
 
 <template>
-  {{ loading }}
-  <h1>custom page testing</h1>
+  <h1 class="text-center">custom page testing</h1>
+  <div class="row m-20">
+    <div class="col span-6">
+      <LabeledInput
+        ref="nameInput1"
+        key="name1"
+        v-model:value="name"
+        label="NameLabel 1"
+        placeholder="Type your name"
+        :disabled="false"
+        :min-height="30"
+        :required="true"
+      />
+    </div>
+
+    <div class="col span-6">
+      <LabeledInput
+        ref="nameInput2"
+        key="name2"
+        v-model:value="name"
+        label="NameLabel 2"
+        placeholder="Type your name"
+        :disabled="false"
+        :min-height="30"
+        :required="true"
+      />
+    </div>
+  </div>
+
   <p v-if="loading">Loading...</p>
   <pre v-else>{{ userData }}</pre>
 </template>
